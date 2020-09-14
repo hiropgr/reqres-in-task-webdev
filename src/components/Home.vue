fdbdbc5d3e7c4d5358026e53cebfab59155f8e76
Reyhaz <karasta86@gmail.com>
2020-09-14 22:37:52 +0500

    update content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>CRUD users home</h1>
+    <div>
+        <h1>CRUD users home</h1>
+        <p>Тестовое задание Фронтэнд - разработчик</p>
+        <a target="_blank" href="https://github.com/hiropgr/reqres-in-task-webdev">Страница на github</a>
+    </div>
 </template>
 
 <script>
